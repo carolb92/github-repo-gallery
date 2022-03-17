1c0a89f058be011de62df6143d7932fcb9d90070
Carol B <carolb@bu.edu>
2022-03-17 18:14:45 -0400

    Fetched and displayed repo info

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const profileInfo = document.querySelector(".overview");
 
 const username = "carolb92";
 
+// display repo list
+const repoList = document.querySelector(".repo-list");
+
 const getUserData = async function () {
     const response = await fetch(`https://api.github.com/users/${username}`);
     const userData = await response.json();
@@ -25,4 +28,21 @@ const userInfo = function (userData) {
             <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
         </div>`;
         profileInfo.append(userInfoDiv);
+    getRepos();
+};
+
+const getRepos = async function () {
+    const reposRes = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repos = await reposRes.json();
+    console.log(repos);
+    repoInfo(repos);
+};
+
+const repoInfo = function (repos) {
+    for (let item of repos) {
+        let li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3> ${item.name} </h3>`;
+        repoList.append(li);
+    }
 };
